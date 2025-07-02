742b5f0e0be30cc627291c98c5402422c84fbb4e
Nanicalog <farlany.domingues@estudante.ifms.edu.br>
2025-07-02 09:26:16 -0300

    #fiz uma página para o usruaruio mas não está aparecendo

--- src/principal.tsx
+++ src/principal.tsx
@@ -1,3 +1,30 @@
-import "./Pagina.css";
+import './pagina.css';
 
-function Principal{}
+function principal() {
+    return (
+        <>
+            <header className="cabecalho">
+                
+                <div className="logotipo"> Plumeria</div>
+                <nav className="navegacao">
+                    <ul className="links-navegacao">
+                        <li><a href="/perfumes">Perfumes</a></li>
+                        <li><a href="/nova fragancia">Nova Fagância</a></li>
+                        <li><a href="/login">Cadastrar Produtos</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <main>
+                <h1>A melhor perfumaria!</h1>
+                <h2>Perfumes da melhor fragância para voçê.</h2>
+            </main>
+
+            <footer className="rodape">
+                <p>&copy; 2025 Plumeria – Todos os direitos reservados.</p>
+            </footer>
+        </>
+    )
+}
+
+export default principal;
