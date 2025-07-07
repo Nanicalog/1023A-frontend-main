5b33c989aa5bed1e6ba698185c11f9786a89e446
Nanicalog <farlany.domingues@estudante.ifms.edu.br>
2025-07-07 10:46:31 -0300

    #pagina de rosto

--- src/principal.tsx
+++ src/principal.tsx
@@ -1,30 +1,88 @@
 import './pagina.css';
 
-function principal() {
-    return (
-        <>
-            <header className="cabecalho">
-                
-                <div className="logotipo"> Plumeria</div>
-                <nav className="navegacao">
-                    <ul className="links-navegacao">
-                        <li><a href="/perfumes">Perfumes</a></li>
-                        <li><a href="/nova fragancia">Nova Fagância</a></li>
-                        <li><a href="/login">Cadastrar Produtos</a></li>
-                    </ul>
-                </nav>
-            </header>
+function Principal() {
+  return (
+    <>
+      <header className="cabecalho">
+        <div className="logotipo">Plumeria</div>
+        <nav className="navegacao">
+          <ul className="links-navegacao">
+            <li><a href="/perfumes">Perfumes</a></li>
+            <li><a href="/nova-fragrancia">Nova Fragrância</a></li>
+            <li><a href="/login">Cadastrar Produtos</a></li>
+          </ul>
+        </nav>
+      </header>
 
-            <main>
-                <h1>A melhor perfumaria!</h1>
-                <h2>Perfumes da melhor fragância para voçê.</h2>
-            </main>
+      <main>
+        <section className="novidades">
+          <div className="produto">
+            <div className="imagem">
+              <img
+                src="https://acdn-us.mitiendanube.com/stores/001/487/363/products/71wwanx59kl-f133ac9272d2e56ccb17480290026030-1024-1024.png"
+                alt=">Explosão doce e envolvente que desperta os sentidos com charme e ousadia."
+              />
+            </div>
+            <h2>Sweet Tooth</h2>
+            <p>Explosão doce e envolvente que desperta os sentidos com charme e ousadia.</p>
+            <strong>$400.50</strong>
+          </div>
+        </section>
+        
 
-            <footer className="rodape">
-                <p>&copy; 2025 Plumeria – Todos os direitos reservados.</p>
-            </footer>
-        </>
-    )
+           <section className="novidades">
+          <div className="produto">
+            <div className="imagem">
+              <img
+                src="https://drogariaspacheco.vteximg.com.br/arquivos/ids/981577-1000-1000/image-1eba1445112b4c9ebf7ef5dc660c5987.jpg?v=638004925121800000"
+                alt="Um convite à paixão intensa"
+              />
+            </div>
+            <h2>Mon Pariz</h2>
+            <p>Um convite à paixão intensa — uma fragrância que captura o arrepio do amor à primeira vista.</p>
+            <strong>$800.00</strong>
+          </div>
+        </section>
+
+        
+           <section className="novidades">
+          <div className="produto">
+            <div className="imagem">
+              <img
+                src="https://dcdn-us.mitiendanube.com/stores/002/272/909/products/ariana-thank-031-ba00c99068ec068d6816618102060919-1024-1024.jpg"
+                alt="— doce, confiante e ousada."
+              />
+            </div>
+            <h2>Thank U Next - Ariana Grande</h2>
+            <p>Thank U Next — doce, confiante e feita para quem escreve sua própria história.</p>
+            <strong>$300.00</strong>
+          </div>
+        </section>
+
+           <section className="novidades">
+          <div className="produto">
+            <div className="imagem">
+              <img
+                src="https://images.tcdn.com.br/img/img_prod/611043/miss_dior_perfume_feminino_edp_50ml_dior_6349_1_0b303ade70f98b1992e3a1deb4d404aa.jpg"
+                alt="elegância em forma de aroma<"
+              />
+            </div>
+            <h2>Miss Dior</h2>
+            <p>Miss Dior — elegância em forma de aroma</p>
+            <strong>$300.00</strong>
+          </div>
+        </section>
+
+
+        <h1>A melhor perfumaria!</h1>
+        <p>Perfumes da melhor fragrância para você.</p>
+      </main>
+
+      <footer className="rodape">
+        <p>&copy; 2025 Plumeria – Todos os direitos reservados.</p>
+      </footer>
+    </>
+  );
 }
 
-export default principal;
+export default Principal;
